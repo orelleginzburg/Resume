beaf920d428b9398fa644bbb0cbf9f074ad1f233
orelleginzburg <73662221+orelleginzburg@users.noreply.github.com>
2021-10-24 18:14:28 -0400

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+/* Open */
+
+function openNav() {
+   document.getElementById("myNav").style.display = "block";
+   document.getElementById("beforeNav").style.display = "none";
+   document.body.style.background = "#1c4ca4";
+}
+
+/* Close */
+function closeNav() {
+   document.getElementById("myNav").style.display = "none";
+   document.getElementById("beforeNav").style.display = "block";
+   document.body.style.background = "#ffffff";
+}
